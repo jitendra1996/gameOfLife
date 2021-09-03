6e42e4b9e0738dd6ae4520c582158937814bff96
jitendra1996 <jitendr199618@gmail.com>
2021-09-03 10:49:05 +0530

    config tweak in webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         filename : 'js/bundle.js'
     },
     devServer : {
-        contentBase : './public'
+        static : './public'
     },
     plugins : [
         new HtmlWebpackPlugin({
