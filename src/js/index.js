3a6e5acc9979cd3d4ed9dc8128140163a78bda7e
jitendra1996 <jitendr199618@gmail.com>
2021-09-04 18:15:33 +0530

    style to btns

--- src/js/index.js
+++ src/js/index.js
@@ -24,9 +24,9 @@ elements.canvas.addEventListener("click", (e) => {
 
 //play btn
 elements.playBtn.addEventListener("click", (e) => {
-  console.log("clicked play btn");
+  //console.log("clicked play btn");
   GamePlay.GAME_STATUS = true;
-  console.log(GamePlay.GAME_STATUS);
+  //console.log(GamePlay.GAME_STATUS);
     if(GamePlay.GAME_STATUS && !GAME_RUNNING_STATUS){
     gameWorld.gamePlay();
     GAME_RUNNING_STATUS = true ;
@@ -35,78 +35,92 @@ elements.playBtn.addEventListener("click", (e) => {
 
 //stop btn
 elements.pauseBtn.addEventListener("click", (e) => {
-  console.log("clicked stop btn");
+  //console.log("clicked stop btn");
   GAME_RUNNING_STATUS = false ;
   GamePlay.GAME_STATUS = false ;
-  console.log(GamePlay.GAME_STATUS);
+  //console.log(GamePlay.GAME_STATUS);
 });
 
 //incease speed of animation frame
 elements.speedUpBtn.addEventListener("click", (e) => {
-  console.log("clicked move fast btn");
-  if (GamePlay.RATE_OF_FRAME > 100) {
-    GamePlay.RATE_OF_FRAME -= 100;
-    console.log(GamePlay.RATE_OF_FRAME);
-  } else {
-    alert("Max Limit");
+  //console.log("clicked move fast btn");
+  if(GamePlay.GAME_STATUS){
+    if (GamePlay.RATE_OF_FRAME > 100) {
+      GamePlay.RATE_OF_FRAME -= 100;
+      console.log(GamePlay.RATE_OF_FRAME);
+    } else {
+      alert("Max Limit");
+    }
   }
 });
 
 //decrease speed of animation frame
 elements.speedDownBtn.addEventListener("click", (e) => {
-  console.log("clicked move slow btn");
-  if (GamePlay.RATE_OF_FRAME < 7000) {
-    GamePlay.RATE_OF_FRAME += 100;
-    console.log(GamePlay.RATE_OF_FRAME);
-  } else {
-    alert("Min Limit");
+  //console.log("clicked move slow btn");
+  if(GamePlay.GAME_STATUS){
+    if (GamePlay.RATE_OF_FRAME < 7000) {
+      GamePlay.RATE_OF_FRAME += 100;
+      console.log(GamePlay.RATE_OF_FRAME);
+    } else {
+      alert("Min Limit");
+    }
   }
 });
 
 //increase size of grid/cell
 elements.incrementGridSizeBtn.addEventListener("click", (e) => {
-  console.log("clicked increase grid size btn");
-  if (GRID_SIZE <= 50) {
-    CellView.width += 10;
-    CellView.height += 10;
-    GRID_SIZE += 10;
-  } else {
-    alert("Max Grid Size");
+  //console.log("clicked increase grid size btn");
+  if(GamePlay.GAME_STATUS){
+    if (GRID_SIZE <= 50) {
+      CellView.width += 10;
+      CellView.height += 10;
+      GRID_SIZE += 10;
+    } else {
+      alert("Max Grid Size");
+    }
   }
 });
 
 //decrease size of grid/cell
 elements.decrementGridSizeBtn.addEventListener("click", (e) => {
-  console.log("clicked decrease grid size btn");
-  if (GRID_SIZE > 10) {
-    CellView.width -= 10;
-    CellView.height -= 10;
-    GRID_SIZE -= 10;
-  } else {
-    alert("Min Grid Size");
+  //console.log("clicked decrease grid size btn");
+  if(GamePlay.GAME_STATUS){
+    if (GRID_SIZE > 10) {
+      CellView.width -= 10;
+      CellView.height -= 10;
+      GRID_SIZE -= 10;
+    } else {
+      alert("Min Grid Size");
+    }
   }
 });
 
 //Randomly selecting Alive grid
 elements.randomlySelectingGridBtn.addEventListener("click", (e) => {
-  console.log("clicked randomly selecting grid btn");
-  let objectArr = gameWorld.getGameObject();
-  let randomLocation = Math.floor(Math.random()*objectArr.length);
-  if(objectArr[randomLocation].alive){
-      CellView.COLOR_FOR_RANDOM_SELECTED_CELL += '#8a2be2';
-      objectArr[randomLocation].context.fillStyle = '#8a2be2' ;
-      console.log(objectArr[randomLocation].context)
+  //console.log("clicked randomly selecting grid btn");
+  if(GamePlay.GAME_STATUS){
+    let objectArr = gameWorld.getGameObject();
+    let randomLocation = Math.floor(Math.random()*objectArr.length);
+  
+    if(objectArr[randomLocation].alive){
+        CellView.COLOR_FOR_RANDOM_SELECTED_CELL += '#8a2be2';
+        objectArr[randomLocation].context.fillStyle = '#8a2be2' ;
+        GamePlay.GAME_STATUS = false ;
+        console.log(objectArr[randomLocation])
+    }
+    console.log(objectArr[randomLocation].context.fillStyle);
+    console.log(objectArr[randomLocation].COLOR_FOR_LIVE);
+    console.log(objectArr[randomLocation].alive);
+    console.log(objectArr.length);
+  
   }
-  console.log(objectArr[randomLocation].context.fillStyle);
-  console.log(objectArr[randomLocation].alive);
-  console.log(objectArr.length);
 });
 
 
 // console.log(GamePlay.GAME_STATUS);
 
 // //gameObject values
-// let objectArr = gameWorld.getGameObject();
+let objectArr = gameWorld.getGameObject();
 // // console.log(objectArr);
 // // console.log(objectArr[0]);
 // // console.log(objectArr[0].alive);
--- src/js/views/CellView.js
+++ src/js/views/CellView.js
@@ -2,21 +2,19 @@ export default class CellView {
 
   static width = 10 ;
   static height = 10 ;
-  static COLOR_FOR_LIVE = "#6495ed" ;
-  static COLOR_FOR_DEAD = "#000000" ;
 
   constructor(context, xPoint, yPoint) {
     this.context = context;
     this.xPoint = xPoint;
     this.yPoint = yPoint;
 
-    this.alive = Math.random() < 0.5;
-    // this.width = 60;
-    // this.height = 60;
+    this.alive = Math.random() > 0.5;
+    this.COLOR_FOR_LIVE = "#6495ed" ;
+    this.COLOR_FOR_DEAD = "#000000" ;
   }
 
   drawSquare() {
-    this.context.fillStyle = this.alive ? CellView.COLOR_FOR_LIVE : CellView.COLOR_FOR_DEAD;
+    this.context.fillStyle = this.alive ? this.COLOR_FOR_LIVE : this.COLOR_FOR_DEAD;
     this.context.fillRect(
       this.xPoint * CellView.width,
       this.yPoint * CellView.height,
